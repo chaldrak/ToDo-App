6037ed56cbbfbbd54c6de472300bed821f0a2c26
Chaldrak <chaldrakus@gmail.com>
2022-10-07 23:24:38 +0100

    display the todos

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ window.onload = function () {
     input.value = "";
 
     let todos_local = localStorage.getItem("todos");
+    
     if (todos_local) {
         todos_tab = JSON.parse(todos_local);
+        todos_tab.forEach(todo => {
+            display(todo);
+        });
     }
     else {
         todos_tab = [];
@@ -24,8 +28,10 @@ window.onload = function () {
         localStorage.setItem("todos", JSON.stringify(todos_tab));
         input.value = ""
         input_count.innerHTML = 0;
-        isInput(input)
-    })
+        isInput(input);
+        display(todos_tab.at(todos_tab.length-1));
+    });
+
     
     // let todos = document.getElementById("todos");
     // let todo_card = document.getElementById("todo-card");
@@ -49,13 +55,31 @@ window.onload = function () {
         }
     };
 
-    todos_tab.forEach(todo => {
-        let div = document.createElement("div");
+    function display (todo) {
+        let div1 = document.createElement("div");
+        let div2 = document.createElement("div")
         let p = document.createElement("p");
+        let input = document.createElement("input");
+        let btn = document.createElement("button");
         const node = document.createTextNode(todo);
 
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("class", "checkbox border border-gray-50");
+        
         p.appendChild(node);
-        div.appendChild(p);
-        todos.appendChild(div);
-    });
+        
+        div2.appendChild(input);
+        div2.appendChild(p);
+        div2.classList.add("w-full", "items-center", "flex", "space-x-5");
+        
+        btn.innerHTML = "<i class='fa fa-trash'></i>";
+        btn.setAttribute("class", "btn text-gray-50 btn-error btn-xs");
+        btn.setAttribute("id", "delete" + Math.round(Math.random() * 10E9));
+
+        div1.appendChild(div2);
+        div1.appendChild(btn);
+        div1.classList.add("w-full", "bg-gradient-to-r", "from-indigo-400", "via-purple-400", "to-indigo-400", "px-5", "py-2", "inline-flex", "items-center", "justify-between", "rounded-md", "shadow-sm", "mb-2");
+        
+        todos.appendChild(div1);
+    };
 }
